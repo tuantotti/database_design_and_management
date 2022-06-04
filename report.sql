f78e0c4b79015a5106dae419fd43b15afbce973c
tuantotti <tuantotti14032001@gmail.com>
2022-06-04 17:11:31 +0700

    done

---
+++ report.sql
@@ -0,0 +1,10 @@
+﻿USE AdventureWorksDW2019
+
+--Không sử dụng Index và sử dụng Index
+SELECT * INTO DimProduct_NoIndex FROM DimProduct
+SELECT * INTO DimProduct_Index FROM DimProduct
+
+CREATE INDEX Idx_DimProduct_Index_EnglishProductName ON DimProduct_Index(EnglishProductName);
+
+SELECT * FROM DimProduct_NoIndex WHERE EnglishProductName LIKE 'Adjustable Race'
+SELECT * FROM DimProduct_Index WHERE EnglishProductName LIKE 'Adjustable Race'
